ccfededc70b0afb7a6883eade2d7649729dfd895
Maxim <maxl08@list.ru>
2023-12-10 15:28:12 +0300

    HW3

---
+++ Main.java
@@ -0,0 +1,36 @@
+package Seminar.Sem3.OOP_DZ3;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PharmacyComponent azitronit = new Azitronit("Azitronit","10mg,",50);
+        PharmacyComponent pinicil = new Water("Pinicil","10mg,",50);
+        PharmacyComponent water = new Pinicilin("Water","10mg,",50);
+
+        Pharmacy pharmacy1 = new Pharmacy().addComponent(pinicil).addComponent(water);
+        Pharmacy pharmacy2 = new Pharmacy().addComponent(pinicil).addComponent(azitronit);
+        Pharmacy pharmacy3 = new Pharmacy().addComponent(pinicil).addComponent(water).addComponent(azitronit);
+        Pharmacy pharmacy4 = new Pharmacy().addComponent(azitronit).addComponent(water).addComponent(azitronit);
+        Pharmacy pharmacy5 = new Pharmacy().addComponent(pinicil).addComponent(water).addComponent(azitronit);
+        Pharmacy pharmacy6 = new Pharmacy().addComponent(azitronit).addComponent(water).addComponent(pinicil);
+        Pharmacy pharmacy7 = new Pharmacy().addComponent(pinicil).addComponent(water).addComponent(pinicil);
+        Iterator<PharmacyComponent> iterator = pharmacy1;
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        Set<Pharmacy> result = new HashSet<>();
+        result.add(pharmacy1);
+        result.add(pharmacy2);
+        result.add(pharmacy3);
+        result.add(pharmacy4);
+        result.add(pharmacy5);
+        result.add(pharmacy6);
+        result.add(pharmacy7);
+        System.out.println(pharmacy6.compareTo(pharmacy7));
+        System.out.println(result.size());
+    }
+}
---
+++ Pinicilin.java
@@ -0,0 +1,7 @@
+package Seminar.Sem3.OOP_DZ3;
+
+public class Pinicilin extends PharmacyComponent {
+    public Pinicilin(String name, String weight, int power) {
+        super(name, weight, power);
+    }
+}
